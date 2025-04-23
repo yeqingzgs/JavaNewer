fedee71d59fead27a67c6dbd76da800d917fe755
MuMing <639554728@qq.com>
2025-04-23 19:16:46 +0800

    update

---
+++ src/xyz/x1Star/MuMing/Switchjava17.java
@@ -0,0 +1,16 @@
+package xyz.x1Star.MuMing;
+
+public class Switchjava17 {
+    public static void main(String[] args) {
+        int day = 3;
+        switch (day) {
+            case 1 -> System.out.println("星期一");
+            case 2 -> System.out.println("星期二");
+            case 3 -> System.out.println("星期三");
+            case 4 -> System.out.println("星期四");
+            case 5 -> System.out.println("星期五");
+            case 6 -> System.out.println("星期六");
+            case 7 -> System.out.println("星期日");
+        }
+    }
+}
