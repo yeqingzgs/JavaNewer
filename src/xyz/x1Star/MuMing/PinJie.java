7008eed844781317d044bf6bc9f237fdcd79d70a
MuMing <639554728@qq.com>
2025-04-23 19:19:44 +0800

    update

---
+++ src/xyz/x1Star/MuMing/PinJie.java
@@ -0,0 +1,11 @@
+package xyz.x1Star.MuMing;
+
+public class PinJie {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        String abc = a + b + c;
+        System.out.println(abc); // 输出拼接结果
+    }
+}
